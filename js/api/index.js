bfed5b2896e15b284433dde427c31a751a791baf
Matt Nawrocki <mjnrock@umich.edu>
2020-05-14 14:18:41 -0400

    init

--- js/api/index.js
+++ js/api/index.js
@@ -4,9 +4,16 @@ import fs from "fs";
 const APP = express();
 const PORT = 3001;
 
+/**
+ * This is a newer way to do the work commonly seen with `bodyParser`
+ */
 APP.use(express.urlencoded({ extended: true }));
 APP.use(express.json());
 APP.use(express.raw());
+
+/**
+ * This activates CORS
+ */
 APP.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -14,6 +21,9 @@ APP.use((req, res, next) => {
     next();
 });
 
+/**
+ * A sample GET request that reads the /data/messages.json file and sends it back as a response
+ */
 APP.get("/feed", (req, res) => {
     // "/feed/:feedId"
     // req.params.feedId
@@ -21,6 +31,10 @@ APP.get("/feed", (req, res) => {
         return res.send(buff.toString());
     });
 });
+
+/**
+ * A sample POST request that will open the /data/messages.json file, append a new message, save and close the file, and return a 200 response code (i.e. "OK")
+ */
 APP.post("/message/", (req, res) => {
     const filepath = "./data/messages.json";
     const message = req.body;
@@ -42,6 +56,9 @@ APP.post("/message/", (req, res) => {
     });
 });
 
+/**
+ * This causes the `express` library to invoke a server on port=@PORT, with a console.log callback
+ */
 APP.listen(PORT, () =>
     console.log(`Example app listening on port ${PORT}!`),
 );
